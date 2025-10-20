fc44a258c088b464e9709abdad818988efbcd89a
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-10-20 08:15:16 +0530

    add user model with password hashing and validation

---
+++ server/src/models/User.ts
@@ -0,0 +1,49 @@
+import bcrypt from "bcryptjs";
+import mongoose, { Document, Schema, type InferSchemaType } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    fullname: { type: String, required: true, trim: true },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    bio: {
+      type: String,
+      trim: true,
+      maxLength: 500,
+    },
+    avatar: { type: String },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+export type IUser = InferSchemaType<typeof userSchema>;
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+export default mongoose.model<IUser>("User", userSchema);
