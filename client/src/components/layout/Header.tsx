f1693e5c0d264cc98811b450e79aa216632e4c42
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-10-29 06:24:43 +0530

    implement scroll effect in Header component for dynamic background color

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
 import Button from "../common/Button";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrolled]);
+
   return (
-    <div className="fixed top-0 left-0 z-99 flex w-full items-center justify-between px-3 py-2">
+    <div
+      className={`fixed top-0 left-0 z-99 flex w-full items-center justify-between px-3 py-2 transition-all ${scrolled ? "bg-maroon text-white" : "text-saffron bg-transparent"}`}
+    >
       <img src={logo} alt="logo" className="h-12 w-12" />
-      <div className="text-saffron flex items-center gap-10 text-lg font-semibold">
+      <div className="flex items-center gap-10 text-lg font-semibold">
         <a href="/">Philosophy</a>
         <a href="/">Science</a>
         <a href="/">Culture</a>
@@ -17,7 +36,5 @@ const Header = () => {
     </div>
   );
 };
-
-// #902c14
 
 export default Header;
