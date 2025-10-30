597c76223c85ade85f7ed7b00ba7b8ba9de4c862
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-10-30 11:16:12 +0530

    fix: change verify-email route method from POST to GET

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post("/signUp", signUp);
 router.post("/signIn", signIn);
-router.post("/verify-email", verifyEmail);
+router.get("/verify-email", verifyEmail);
 router.post("/resend-verification", resendVerification);
 
 export default router;
