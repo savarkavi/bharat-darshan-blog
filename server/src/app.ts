53a09bdc76f4a1e8898460d954875c4ea3fb5e8b
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-11 15:20:52 +0530

    feat: add cookie-parser middleware for handling cookies and improve CORS configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/authRoutes.ts";
 import blogRoutes from "./routes/blogRoutes.ts";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
