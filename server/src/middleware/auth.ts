39adbfecfc81999f63c3ca51de46b794571b4344
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-12 08:31:11 +0530

    changed message for invalid token in the middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -15,7 +15,7 @@ export const protect = async (
     const token = req.cookies.token;
 
     if (!token) {
-      return res.status(401).json({ message: "Not authorized" });
+      return res.status(401).json({ message: "Not authenticated" });
     }
 
     const decodedToken = jwt.verify(
