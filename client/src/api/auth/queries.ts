8d4955c3a36a7f765698120e21a7070a14559e7b
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-12 06:07:41 +0530

    refactor: remove useAuthStore and integrate react-query for authentication management

--- client/src/api/auth/queries.ts
+++ client/src/api/auth/queries.ts
@@ -1,37 +1,47 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { authService } from "../../services/authService";
-import { useAuthStore } from "../../store/useAuth";
+import { toast } from "react-toastify";
+import type { AxiosError } from "axios";
+import type { ApiError } from "../../types/global";
+import { useNavigate } from "react-router-dom";
 
 export const useAuthUser = () => {
-  return useQuery({
+  const query = useQuery({
     queryKey: ["auth", "user"],
     queryFn: authService.fetchAuthUser,
     retry: false,
     staleTime: Infinity,
   });
+
+  const user = query.data;
+  const isAuthenticated = !!user;
+
+  return { user, isAuthenticated, isLoading: query.isLoading };
 };
 
 export const useSignIn = () => {
-  const setUser = useAuthStore((state) => state.setUser);
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   return useMutation({
     mutationFn: authService.signIn,
     onSuccess: (data) => {
-      setUser(data);
       queryClient.setQueryData(["auth", "user"], data);
+      navigate("/");
+    },
+    onError: (error: AxiosError<ApiError>) => {
+      console.log(error.response?.data.message);
+      toast.error(error.response?.data.message);
     },
   });
 };
 
 export const useSignUp = () => {
-  const setUser = useAuthStore((state) => state.setUser);
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: authService.signUp,
     onSuccess: (data) => {
-      setUser(data);
       queryClient.setQueryData(["auth", "user"], data);
     },
   });
