4c006d26d3c9e0425f0188acf48c8b9f3ed44863
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-16 07:00:50 +0530

    use cn function for Button component

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,4 +1,5 @@
 import type { ButtonHTMLAttributes, ReactNode } from "react";
+import { cn } from "../../lib/utils";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
@@ -8,7 +9,10 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 const Button = ({ children, className, ...props }: ButtonProps) => {
   return (
     <button
-      className={`bg-saffron flex cursor-pointer items-center justify-center rounded-sm px-5 py-1 text-white hover:opacity-80 ${className}`}
+      className={cn(
+        "bg-saffron flex cursor-pointer items-center justify-center rounded-sm px-5 py-1 text-white hover:opacity-80",
+        className,
+      )}
       {...props}
     >
       {children}
