1e9d3de45e54b58d6cafe616f3c1e8a768427bd2
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-19 08:34:24 +0530

    used cn function for dynamic classes in Toolbar button

--- client/src/components/toolbar/ToolbarButton.tsx
+++ client/src/components/toolbar/ToolbarButton.tsx
@@ -1,4 +1,5 @@
 import type { IconType } from "react-icons/lib";
+import { cn } from "../../lib/utils";
 
 interface ToolbarButtonProps {
   icon: IconType;
@@ -16,7 +17,10 @@ const ToolbarButton = ({
   return (
     <button
       onClick={onClick}
-      className={`hover:bg-light-parchment cursor-pointer rounded-md p-1 ${isActive && "bg-light-parchment"} `}
+      className={cn(
+        "hover:bg-light-parchment cursor-pointer rounded-md p-1",
+        isActive && "bg-light-parchment",
+      )}
     >
       <Icon className={iconSize ? iconSize : "size-5"} />
     </button>
