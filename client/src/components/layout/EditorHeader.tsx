da8f1a0007df736d6b8e23a5d83f18961a3dc233
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-19 08:33:45 +0530

    feat: enhance EditorHeader with save draft functionality and loading state

--- client/src/components/layout/EditorHeader.tsx
+++ client/src/components/layout/EditorHeader.tsx
@@ -1,11 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import { IoMdEye } from "react-icons/io";
 import { FaSave } from "react-icons/fa";
 import { MdOutlinePublish } from "react-icons/md";
 import Button from "../common/Button";
+import { useFormContext, type SubmitHandler } from "react-hook-form";
+import type { IFormInput } from "../../layouts/EditorLayout";
+import { useEditorStore } from "../../store/use-editor";
+import { useSaveDraft } from "../../api/blog/blogApi";
+import { ImSpinner2 } from "react-icons/im";
 
 const EditorHeader = () => {
+  const { handleSubmit } = useFormContext<IFormInput>();
+  const editor = useEditorStore((state) => state.editor);
+  const { mutate, isPending } = useSaveDraft();
+  const { slug } = useParams();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    mutate({
+      data: {
+        title: data.essayTitle,
+        excerpt: data.excerpt,
+        category: data.category,
+        content: editor?.getJSON(),
+        coverImage: "adfsd",
+        tags: ["hi", "there"],
+      },
+      slug,
+    });
+  };
+
   return (
     <div className="bg-light-parchment flex items-center justify-between px-4 py-2">
       <Link to={"/"}>
@@ -16,9 +40,18 @@ const EditorHeader = () => {
           <IoMdEye />
           <span>Preview</span>
         </Button>
-        <Button className="bg-saffron flex items-center gap-2">
-          <FaSave />
-          <span>Save Draft</span>
+        <Button
+          onClick={handleSubmit(onSubmit)}
+          className="bg-saffron flex h-8 w-32 items-center justify-center"
+        >
+          {isPending ? (
+            <ImSpinner2 className="animate-spin" />
+          ) : (
+            <div className="flex w-full items-center gap-2">
+              <FaSave />
+              <span>Save Draft</span>
+            </div>
+          )}
         </Button>
         <Button className="bg-maroon flex items-center gap-2">
           <MdOutlinePublish />
