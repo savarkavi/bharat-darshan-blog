e758b75e9787251cdfb5d8cb4ac68d17dfbff27c
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-22 10:42:11 +0530

    feat: add FormValidationError component for displaying field validation errors

---
+++ client/src/components/common/FormValidationError.tsx
@@ -0,0 +1,27 @@
+import type { FieldError } from "react-hook-form";
+import { MdError } from "react-icons/md";
+
+interface RequiredErrorProps {
+  field: FieldError | undefined;
+  errorType: string;
+  message: string;
+}
+
+const FormValidationError = ({
+  field,
+  errorType,
+  message,
+}: RequiredErrorProps) => {
+  return (
+    field?.type === errorType && (
+      <div className="flex items-center gap-1 text-red-500">
+        <MdError />
+        <p role="alert" className="text-lg text-red-600">
+          {message}
+        </p>
+      </div>
+    )
+  );
+};
+
+export default FormValidationError;
