1299bc333b6abef3340d856b1c522a29a35aa621
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-24 14:56:25 +0530

    removed required prop for several fields in blogSchema

--- server/src/models/Blog.ts
+++ server/src/models/Blog.ts
@@ -3,12 +3,12 @@ import mongoose, { Schema, type InferSchemaType } from "mongoose";
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true },
   slug: { type: String, required: true, unique: true },
-  excerpt: { type: String, required: true, trim: true, maxLength: 200 },
-  content: { type: Object, required: true },
-  coverImage: { type: String, required: true },
+  excerpt: { type: String, trim: true, maxLength: 200 },
+  content: { type: Object },
+  coverImage: { type: String },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   tags: [{ type: String, lowercase: true }],
-  category: { type: String, required: true },
+  category: { type: String },
   status: { type: String, enum: ["draft", "published"], default: "draft" },
 });
 
