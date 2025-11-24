9a99c5442b9ea7d4c2767fc9f115a103746ae883
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-24 14:54:11 +0530

    feat: implement file upload middleware using Cloudinary and multer for blog cover images

--- server/src/routes/blogRoutes.ts
+++ server/src/routes/blogRoutes.ts
@@ -9,18 +9,19 @@ import {
 } from "../controllers/blogController.ts";
 import { protect } from "../middleware/auth.ts";
 import { verifiedOnly } from "../middleware/verifyEmail.ts";
+import { upload } from "../middleware/upload.ts";
 
 const router = express.Router();
 
 router.route("/").get(getAllBlogs).post(protect, verifiedOnly, createBlog);
 
-router.post("/draft", protect, saveDraft);
-router.put("/draft/:slug", protect, saveDraft);
+router.post("/draft", protect, upload.single("coverImage"), saveDraft);
+router.put("/draft/:slug", protect, upload.single("coverImage"), saveDraft);
 
 router
   .route("/:slug")
   .get(getBlogBySlug)
-  .put(protect, verifiedOnly, updateBlog)
+  .put(protect, verifiedOnly, upload.single("coverImage"), updateBlog)
   .delete(protect, verifiedOnly, deleteBlog);
 
 export default router;
