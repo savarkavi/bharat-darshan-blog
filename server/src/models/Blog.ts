27fb52928a9ddf49163e2ba0df09f2a2d190b6eb
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-29 10:44:43 +0530

    feat: update blogSchema to include reviewPending status and enable timestamps

--- server/src/models/Blog.ts
+++ server/src/models/Blog.ts
@@ -1,16 +1,23 @@
 import mongoose, { Schema, type InferSchemaType } from "mongoose";
 
-const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: true },
-  slug: { type: String, required: true, unique: true },
-  excerpt: { type: String, trim: true, maxLength: 200 },
-  content: { type: Object },
-  coverImage: { type: String },
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  tags: [{ type: String, lowercase: true }],
-  category: { type: String },
-  status: { type: String, enum: ["draft", "published"], default: "draft" },
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    slug: { type: String, required: true, unique: true },
+    excerpt: { type: String, trim: true, maxLength: 200 },
+    content: { type: Object },
+    coverImage: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    tags: [{ type: String, lowercase: true }],
+    category: { type: String },
+    status: {
+      type: String,
+      enum: ["draft", "reviewPending", "published"],
+      default: "draft",
+    },
+  },
+  { timestamps: true }
+);
 
 export type IBlog = InferSchemaType<typeof blogSchema>;
 
