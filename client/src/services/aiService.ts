468a74fba2c5daf117bca5ff8c70937657309db1
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-12-10 10:58:02 +0530

    feat: modify askPerplexity method to support streaming responses and handle chunked data processing

--- client/src/services/aiService.ts
+++ client/src/services/aiService.ts
@@ -1,13 +1,39 @@
-import api from "./api";
-
 interface AIServiceParams {
   query: string;
   blogSlug: string;
+  onChunk: (chunk: string) => void;
 }
 
 export const aiService = {
-  askPerplexity: async ({ query, blogSlug }: AIServiceParams) => {
-    const res = await api.put("/ai/ask-perplexity", { query, blogSlug });
-    return res.data;
+  askPerplexity: async ({ query, blogSlug, onChunk }: AIServiceParams) => {
+    const res = await fetch(
+      `${import.meta.env.VITE_BASE_URL}/ai/ask-perplexity`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query, blogSlug }),
+      },
+    );
+
+    if (!res.ok || !res.body) {
+      throw new Error(res.statusText);
+    }
+
+    const reader = res.body.getReader();
+    const decoder = new TextDecoder();
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+
+      const text = decoder.decode(value);
+
+      onChunk(text);
+    }
+
+    return "Stream complete";
   },
 };
