e66d6cbf39d211e87a3bd7bbb97aefe067b0df71
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-12-20 11:09:47 +0530

    fix: enhance FormValidationError component to accept custom class names for improved styling flexibility

--- client/src/components/common/FormValidationError.tsx
+++ client/src/components/common/FormValidationError.tsx
@@ -1,20 +1,23 @@
 import type { FieldError } from "react-hook-form";
 import { MdError } from "react-icons/md";
+import { cn } from "../../lib/utils";
 
 interface RequiredErrorProps {
   field: FieldError | undefined;
   errorType: string;
   message: string;
+  classNames?: string;
 }
 
 const FormValidationError = ({
   field,
   errorType,
   message,
+  classNames,
 }: RequiredErrorProps) => {
   return (
     field?.type === errorType && (
-      <div className="flex items-center gap-1 text-red-500">
+      <div className={cn("flex items-center gap-1 text-red-500", classNames)}>
         <MdError className="size-5" />
         <p role="alert" className="text-lg text-red-600">
           {message}
