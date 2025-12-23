95eb86d307e7ab43f6fe65d9d3d00e15e02e1241
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-12-23 06:31:47 +0530

    use actual blog likes and comment data to show in EssayStats instead of hard-coded values

--- client/src/components/essay/EssayStats.tsx
+++ client/src/components/essay/EssayStats.tsx
@@ -1,16 +1,17 @@
 import { CiHeart, CiShare2, CiChat1 } from "react-icons/ci";
+import type { GetBlogBySlugData } from "../../types/types";
 
-const EssayStats = () => {
+const EssayStats = ({ data }: { data: GetBlogBySlugData }) => {
   return (
     <div className="border-ash-grey mx-auto flex w-full max-w-[800px] items-center justify-between border-y py-4">
       <div className="flex items-center gap-6">
         <div className="border-ash-grey flex cursor-pointer items-center gap-2 rounded-full border px-3 py-1 text-lg select-none hover:bg-gray-200">
           <CiHeart className="size-5" />
-          <p>107</p>
+          <p>{data.blog.likes}</p>
         </div>
         <div className="border-ash-grey flex cursor-pointer items-center gap-2 rounded-full border px-3 py-1 text-lg select-none hover:bg-gray-200">
           <CiChat1 className="size-5" />
-          <p>42</p>
+          <p>{data.totalComments}</p>
         </div>
       </div>
       <div className="border-ash-grey flex cursor-pointer items-center gap-2 rounded-full border px-3 py-1 text-lg select-none hover:bg-gray-200">
