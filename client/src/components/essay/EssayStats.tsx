ee661777cff4fe53929d9185f6162b8f9d96a3b9
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-12-28 06:35:37 +0530

    feat: update EssayStats component to include like functionality with user interaction and improved UI elements

--- client/src/components/essay/EssayStats.tsx
+++ client/src/components/essay/EssayStats.tsx
@@ -1,18 +1,51 @@
-import { CiHeart, CiShare2, CiChat1 } from "react-icons/ci";
-import type { GetBlogBySlugData } from "../../types/types";
+import { CiShare2, CiChat1 } from "react-icons/ci";
+import { Heart } from "lucide-react";
+import type { GetBlogBySlugData, User } from "../../types/types";
+import { useLikeBlog } from "../../api/blog/blogApi";
+import { Link, useNavigate } from "react-router-dom";
+import { cn } from "../../lib/utils";
 
-const EssayStats = ({ data }: { data: GetBlogBySlugData }) => {
+interface EssayStatsProps {
+  data: GetBlogBySlugData;
+  user: User | null | undefined;
+}
+
+const EssayStats = ({ data, user }: EssayStatsProps) => {
+  const navigate = useNavigate();
+  const { mutate } = useLikeBlog({ slug: data.blog.slug });
+
+  console.log(user?.userId);
+
+  const isLiked = data.blog.likes.find((id) => id === user?.userId);
+
+  const handleLike = () => {
+    if (!user) {
+      navigate("/sign-in");
+      return;
+    }
+
+    mutate({ userId: user.userId });
+  };
+
   return (
     <div className="border-ash-grey mx-auto flex w-full max-w-[800px] items-center justify-between border-y py-4">
       <div className="flex items-center gap-6">
-        <div className="border-ash-grey flex cursor-pointer items-center gap-2 rounded-full border px-3 py-1 text-lg select-none hover:bg-gray-200">
-          <CiHeart className="size-5" />
-          <p>{data.blog.likes}</p>
+        <div
+          onClick={handleLike}
+          className="border-ash-grey flex cursor-pointer items-center gap-2 rounded-full border px-3 py-1 text-lg select-none hover:bg-gray-200"
+        >
+          <Heart
+            strokeWidth="1px"
+            className={cn("size-5", isLiked && "fill-red-500 stroke-red-500")}
+          />
+          <p>{data.blog.likes.length}</p>
         </div>
-        <div className="border-ash-grey flex cursor-pointer items-center gap-2 rounded-full border px-3 py-1 text-lg select-none hover:bg-gray-200">
-          <CiChat1 className="size-5" />
-          <p>{data.totalComments}</p>
-        </div>
+        <Link to={"comments"}>
+          <div className="border-ash-grey flex cursor-pointer items-center gap-2 rounded-full border px-3 py-1 text-lg select-none hover:bg-gray-200">
+            <CiChat1 className="size-5" />
+            <p>{data.totalComments}</p>
+          </div>
+        </Link>
       </div>
       <div className="border-ash-grey flex cursor-pointer items-center gap-2 rounded-full border px-3 py-1 text-lg select-none hover:bg-gray-200">
         <CiShare2 className="size-5" />
